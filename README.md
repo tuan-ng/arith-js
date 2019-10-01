51c5e3a67bdbcb6638f79dcb7b606a3ddbf7efa6
tuan-ng <iam.tuan.ng@gmail.com>
2019-10-01 12:27:25 -0400

    handles also floats now

--- README.md
+++ README.md
@@ -82,7 +82,7 @@ const parse = tokens => {
   const consume = () => tokens[i++];
 
   const parseAtom = () =>
-    /^[+*/-]$/.test(peek()) ? consume() : parseInt(consume());
+    /^[+*/-]$/.test(peek()) ? consume() : Number(consume());
 
   const parseCompound = () => {
     consume();
--- arith.js
+++ arith.js
@@ -13,7 +13,7 @@ const parse = tokens => {
   const consume = () => tokens[i++];
 
   const parseAtom = () =>
-    /^[+*/-]$/.test(peek()) ? consume() : parseInt(consume());
+    /^[+*/-]$/.test(peek()) ? consume() : Number(consume());
 
   const parseCompound = () => {
     consume();
